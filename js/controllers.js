c78ab8c1cccc058f29120f8ff477440b427e3d22
Mariusz <mariuszz4554@gmail.com>
2016-09-22 22:46:10 +0200

    showStages3

--- js/controllers.js
+++ js/controllers.js
@@ -1230,62 +1230,62 @@
             };
     }])
 
-    app.controller('competitionController', ['$scope','$http', '$location', '$sessionStorage', '$log', function($scope, $http, $location, $sessionStorage, $log){ 
-
-    	$scope.check7 = 0;
-    	
-    	// Editor options.
-        $scope.options = {
-          language: 'pl',
-          allowedContent: true,
-          entities: false
-        };
-
-        // Called when the editor is completely ready.
-        $scope.onReady = function () {
-        	CKEDITOR.instances['editor'].setReadOnly(true);
-        	
-        	var temp = CKEDITOR.instances['editor'].id + "_top";
-        	
-        	var bar = document.getElementById(temp);
-            bar.style.display = "none";
-            
-            temp = CKEDITOR.instances['editor'].id + "_bottom";
-            
-            bar = document.getElementById(temp);
-            bar.style.display = "none";
-        }
-            	
-    	$scope.competition = [];
-        $scope.id = sessionStorage.getItem('compID');
-        
-            $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition?id=' + $scope.id)
-            .success(function(data){
-                $scope.competition = data;
-                
-                if(data.DEACTIVATED == 'true') {
-                	$scope.check7 = 1;
-                }
-
-            })            
-            .error(function(data,status,headers,config){
-                $scope.retInfo = 'Błąd!'+ data;
-            });
-
-            $scope.showCompRunnersList = function(){
-                sessionStorage.setItem('compID', $scope.id);
-                sessionStorage.setItem('compName', $scope.competition.NAME);
-                sessionStorage.setItem('compPay', $scope.competition.OPLATA);
-                $location.path('/Multi/home/competition/compRunnersList');
-
-            }
-            
-           $scope.showResultList = function(){
-                sessionStorage.setItem('compID', $scope.id);
-                $location.path('/Multi/home/competition/results');
-            }      
-            
-    }])
+//    app.controller('competitionController', ['$scope','$http', '$location', '$sessionStorage', '$log', function($scope, $http, $location, $sessionStorage, $log){
+//
+//    	$scope.check7 = 0;
+//
+//    	// Editor options.
+//        $scope.options = {
+//          language: 'pl',
+//          allowedContent: true,
+//          entities: false
+//        };
+//
+//        // Called when the editor is completely ready.
+//        $scope.onReady = function () {
+//        	CKEDITOR.instances['editor'].setReadOnly(true);
+//
+//        	var temp = CKEDITOR.instances['editor'].id + "_top";
+//
+//        	var bar = document.getElementById(temp);
+//            bar.style.display = "none";
+//
+//            temp = CKEDITOR.instances['editor'].id + "_bottom";
+//
+//            bar = document.getElementById(temp);
+//            bar.style.display = "none";
+//        }
+//
+//    	$scope.competition = [];
+//        $scope.id = sessionStorage.getItem('compID');
+//
+//            $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition?id=' + $scope.id)
+//            .success(function(data){
+//                $scope.competition = data;
+//
+//                if(data.DEACTIVATED == 'true') {
+//                	$scope.check7 = 1;
+//                }
+//
+//            })
+//            .error(function(data,status,headers,config){
+//                $scope.retInfo = 'Błąd!'+ data;
+//            });
+//
+//            $scope.showCompRunnersList = function(){
+//                sessionStorage.setItem('compID', $scope.id);
+//                sessionStorage.setItem('compName', $scope.competition.NAME);
+//                sessionStorage.setItem('compPay', $scope.competition.OPLATA);
+//                $location.path('/Multi/home/competition/compRunnersList');
+//
+//            }
+//
+//           $scope.showResultList = function(){
+//                sessionStorage.setItem('compID', $scope.id);
+//                $location.path('/Multi/home/competition/results');
+//            }
+//
+//    }])
 
     app.controller('myCompetitionController', ['$scope','$http', '$location', '$sessionStorage', '$log', function($scope, $http, $location, $sessionStorage, $log){ 
 

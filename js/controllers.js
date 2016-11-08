e3b52b5f96ee8bbf23f141dda1b3cf945dbab765
008M <mariuszz4554@gmail.com>
2016-11-08 19:16:29 +0100

    edycja

--- js/controllers.js
+++ js/controllers.js
@@ -3332,7 +3332,7 @@ app.controller('resultListController', ['$scope','$http', '$route', '$sessionSto
                                                                       $scope.wyniki.push($scope.runners);
 
 
-                                                              $scope.wynikiTimes.push($scope.timesColumn);
+                                                              $scope.wynikiTimes[i].push($scope.timesColumn[i]);
                                                              }
 
 

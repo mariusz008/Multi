02b614fefd820773e35332dd17a6f4c99de6f213
008M <mariuszz4554@gmail.com>
2016-11-11 19:33:24 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -3339,6 +3339,11 @@ app.controller('resultListController', ['$scope','$http', '$route', '$sessionSto
                                                              }
 
                                                          })
+                                                         .then(function(data))
+                                                         {
+                                                         console.log("then");
+                                                         console.log(dd);
+                                                         }
                                                          .error(function(data,status,headers,config){
                                                              $scope.retInfo = 'Błąd!'+ data;
                                                              console.log('Błąd1!'+ data);

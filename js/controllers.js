1ba8de187918a497c295829d00212a9882252ad2
008M <mariuszz4554@gmail.com>
2016-11-17 23:47:58 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -3384,17 +3384,24 @@ var ileZawodnikow = 0;
 $scope.wypelnijKlasyfikacje = function(idZawodow) {
 
 if($scope.daneEtapow!=undefined){
-$http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition/classification?competition_id='+$scope.daneEtapow[idZawodow].COMPETITION_ID)
-                                                         .success(function(data){
+//$http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition/classification?competition_id='+$scope.daneEtapow[idZawodow].COMPETITION_ID)
+//                                                         .success(function(data){
+//
+//                                                        // $scope.classification[1] = [name:'Klasyfikacja generalna'];
+//                                                         $scope.classification[1] = [name:data.TYP];
+//                                                         })
+//                                                         .error(function(data,status,headers,config){
+//                                                                                              $scope.retInfo = 'Błąd!'+ data;
+//                                                                                              console.log('Błąd3!'+ data);
+//                                                                                              });
+ $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition/event/list?competition_id=' + id + '&sex=&age=&phrase=&category=')
+                                    .success(function(data){
+                                        $scope.zawodnicy = data;
+                                    })
 
-                                                        // $scope.classification[1] = [name:'Klasyfikacja generalna'];
-                                                         $scope.classification[1] = [name:data.TYP];
-                                                         })
-                                                         .error(function(data,status,headers,config){
-                                                                                              $scope.retInfo = 'Błąd!'+ data;
-                                                                                              console.log('Błąd3!'+ data);
-                                                                                              });
-
+                                    .error(function(data,status,headers,config){
+                                        $scope.retInfo = 'Błąd!'+ data;
+                                    });
 }}
              //klasyfikacja generalna
             $scope.wynikiGeneralnej = function(idZawodow) {

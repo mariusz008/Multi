b77175c29ddbe7af56b300b70b7b2461d7c70c40
008M <mariuszz4554@gmail.com>
2016-11-17 11:50:19 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -3555,6 +3555,12 @@ sekundy1 = parseInt(sekundy1) + parseInt(eval(timeName).substring(6,8));
                                                                     $scope.runners2.push($filter('orderBy')($scope.runners, 'POINT'+x+'_TIME')[0]);
                                                                     console.log($scope.runners1);
                                                                     console.log($scope.runners2);
+
+                                                                    for(var i = 0; i<$scope.runners.length; i++){
+                                                                        console.log($scope.runners[i]);
+                                                                    console.log($scope.runners1[i]);
+                                                                    console.log($scope.runners2[i]);
+                                                                    }
                                                                  //}
 //                                                                 for(var j=0; j<$scope.runners[0].POINTS_COUNT;j++)
 //                                                                 {

5c85f6659b4eec19a0158ed8dcf470303f90e785
008M <mariuszz4554@gmail.com>
2016-11-18 19:02:56 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -3378,8 +3378,9 @@ $scope.wypelnijKlasyfikacje = function(idZawodow) {
 $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition/classification?competition_id='+$scope.daneEtapow[idZawodow].COMPETITION_ID)
                                                          .success(function(data){
 
-                                                        // $scope.classification[1] = [name:'Klasyfikacja generalna'];
+                                                        // $scope.classification[1] = [name:'Klasyfikacja generalna']
                                                          if(data.TYP != 'Klasyfikacja generalna') $scope.classification[1] = {name:data.TYP};
+
                                                          })
                                                          .error(function(data,status,headers,config){
                                                                                               $scope.retInfo = 'Błąd!'+ data;

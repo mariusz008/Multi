709fb96436807ba27308be9cb3a9c2ac5df5d68a
008M <mariuszz4554@gmail.com>
2016-11-20 15:38:48 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -3280,7 +3280,7 @@ if(id==2){
 
     }])
 
-app.controller('resultListController', ['$scope','$filter', '$http', '$route', '$sessionStorage', '$log', '$location', function($scope, $filter, $http, $route, $sessionStorage, $log, $location){
+app.factory('resultListController', ['$scope','$filter', '$http', '$route', '$sessionStorage', '$log', '$location', function($scope, $filter, $http, $route, $sessionStorage, $log, $location){
 
 $scope.getData = function(ID) {
 
@@ -3713,8 +3713,7 @@ var iter = 0;
                                                              //$scope.getData
                                                               var myDataPromise = $scope.getData($scope.daneEtapow[iter].COMPETITION_ID);
                                                                  myDataPromise.then(function(data) {
-                                                            // $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/result/list?competition_id='+$scope.daneEtapow[iter].COMPETITION_ID)
-                                                            //.success(function(data){
+
                                                              $scope.runners = data;
                                                              $scope.runners2[iter] = data;
                                                              //console.log($scope.runners);

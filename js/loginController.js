f91100a141e08a3a67f4a17e2495c7197021c1cc
008M <mariuszz4554@gmail.com>
2016-11-20 20:23:10 +0100

    wyniki

--- js/loginController.js
+++ js/loginController.js
@@ -16,65 +16,54 @@
         var response = [];
 
         $scope.loginClick = function(){
-        var iter=0;
-        for(iter=0; iter<7; iter++){
-                setInterval(function(){
-                      console.log(iter);
-                },2000);
-        }
-//        if(var==7){
-//        console.log("t");
-//        setInterval(function(){
-//                              console.log(iter);
-//                        },2000);
-//        }
-//            $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/user/login?login='+ $scope.user.login +
-//                                                                        '&password='+ $scope.user.password)
-//
-//            .success(function(data){
-//                response = data;
-//                if(response.ACTIVATE == 'true')
-//                {
-//                	//sessionStorage.setItem('MultiJustRace', response);
-//                	var sex = '';
-//
-//                	if(response.PLEC == 'M'){
-//                		sex = 'Mężczyzna'
-//                	}
-//
-//                	else{
-//                		sex = 'Kobieta';
-//                	}
-//
-//                    sessionStorage.setItem('IMIE', response.IMIE);
-//                    sessionStorage.setItem('ICE', response.ICE);
-//                    sessionStorage.setItem('ACTIVATE', response.ACTIVATE);
-//                    sessionStorage.setItem('OBYWATELSTWO', response.OBYWATELSTWO);
-//                    sessionStorage.setItem('NR_TEL', response.NR_TEL);
-//                    sessionStorage.setItem('NAZWISKO', response.NAZWISKO);
-//                    sessionStorage.setItem('ID', response.ID);
-//                    sessionStorage.setItem('EMAIL', response.EMAIL);
-//                    sessionStorage.setItem('KLUB', response.KLUB);
-//                    sessionStorage.setItem('CODE', response.CODE);
-//                    sessionStorage.setItem('PLEC', sex);
-//                    sessionStorage.setItem('WIEK', response.WIEK);
-//                    sessionStorage.setItem('RememberMe', $scope.user.login);
-//
-//                    window.location = '/Multi/home.html';
-//                }
-//                else
-//                {
-//                    $scope.retInfo = response.MESSAGE;
-//
-//                }
-//
-//
-//            })
-//            .error(function(data,status,headers,config){
-//
-//                $scope.retInfo = 'Błąd!'+ data;
-//
-//            })
+
+            $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/user/login?login='+ $scope.user.login +
+                                                                        '&password='+ $scope.user.password)
+
+            .success(function(data){
+                response = data;
+                if(response.ACTIVATE == 'true')
+                {
+                	//sessionStorage.setItem('MultiJustRace', response);
+                	var sex = '';
+
+                	if(response.PLEC == 'M'){
+                		sex = 'Mężczyzna'
+                	}
+
+                	else{
+                		sex = 'Kobieta';
+                	}
+
+                    sessionStorage.setItem('IMIE', response.IMIE);
+                    sessionStorage.setItem('ICE', response.ICE);
+                    sessionStorage.setItem('ACTIVATE', response.ACTIVATE);
+                    sessionStorage.setItem('OBYWATELSTWO', response.OBYWATELSTWO);
+                    sessionStorage.setItem('NR_TEL', response.NR_TEL);
+                    sessionStorage.setItem('NAZWISKO', response.NAZWISKO);
+                    sessionStorage.setItem('ID', response.ID);
+                    sessionStorage.setItem('EMAIL', response.EMAIL);
+                    sessionStorage.setItem('KLUB', response.KLUB);
+                    sessionStorage.setItem('CODE', response.CODE);
+                    sessionStorage.setItem('PLEC', sex);
+                    sessionStorage.setItem('WIEK', response.WIEK);
+                    sessionStorage.setItem('RememberMe', $scope.user.login);
+
+                    window.location = '/Multi/home.html';
+                }
+                else
+                {
+                    $scope.retInfo = response.MESSAGE;
+
+                }
+
+
+            })
+            .error(function(data,status,headers,config){
+
+                $scope.retInfo = 'Błąd!'+ data;
+
+            })
         }
                        
                     

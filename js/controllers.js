c8fb8733ead5323e6fb9d9b9a1993f46b39c3448
008M <mariuszz4554@gmail.com>
2016-11-22 15:46:02 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -3766,7 +3766,24 @@ $scope.ogolneOdbierz = function(){
                          if(i>0){
                             $scope.ostatniWynikx[i][j].TIMES[0] = $scope.ostatniWynikx[i][j].TIMES[0] + $scope.ostatniWynikx[(i-1)][j].TIMES[0];
                          }
+                         var czas = $scope.ostatniWynikx[i][j].TIMES[0];
+                         var ms = s % 1000;
+                                 s = (s - ms) / 1000;
+                                 var secs = s % 60;
+                                 s = (s - secs) / 60;
+                                 var mins = s % 60;
+                                 var hrs = (s - mins) / 60;
+                         $scope.ostatniWynikx[i][j].TIMES[0] = hrs + ':' + mins + ':' + secs;
                          $scope.ostatniWynikx[i][j].MIEJSCE = i+1;
+                          if($scope.ostatniWynikx[i][j].hasOwnProperty('NAZWISKO'))
+                         {
+                               for(var b=0; b<$scope.zawodnicy.length; b++){
+                                if($scope.ostatniWynikx[i][j].NAZWISKO == $scope.zawodnicy[b].NAZWISKO)
+                                      {
+                                      $scope.ostatniWynikx[i][j].KLUB = $scope.zawodnicy[b].KLUB;
+                                      }
+                                    }
+                               }
                          }
                     }
             }

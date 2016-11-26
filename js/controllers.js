e770d33919833900a29c2be470b3ba4866f0c13e
008M <mariuszz4554@gmail.com>
2016-11-26 14:14:33 +0100

    wyniki

--- js/controllers.js
+++ js/controllers.js
@@ -49,51 +49,6 @@
             $scope.retInfo = 'Błąd!'+ data;
             })
 
-        $http.get('http://209785serwer.iiar.pwr.edu.pl/Rest1/rest/competition/all?type=&name=&place=&wieloetapowe=0')
-                    .success(function(data){
-                    competitionsFilter = data;
-
-                    var datePattern = /(\d{2}).(\d{2}).(\d{4})/;
-                    var counter = 0;
-
-                    for(var i=0; i<competitionsFilter.length; i++)
-                    {
-                        var today = new Date();
-                        var compDate = new Date(competitionsFilter[i].DATA_ROZP.replace(datePattern, '$3, $2, $1'));
-                        var compTime = competitionsFilter[i].CZAS_ROZP.split(":");
-                        compDate.setHours(compTime[0]);
-                        compDate.setMinutes(compTime[1]);
-
-                        if(today <= compDate)
-                        {
-                            $scope.competitions.push(competitionsFilter[i]);
-                            today.setHours(0);
-                            today.setMinutes(0);
-                            today.setSeconds(0);
-                            today.setMilliseconds(0);
-                            compDate.setHours(0);
-                            compDate.setMinutes(0);
-                            compDate.setSeconds(0);
-                            compDate.setMilliseconds(0);
-                            $scope.competitions[counter].DIFF = Math.floor((compDate - today) / (1000*60*60*24))
-                            counter++;
-                        }
-
-                        if(counter>7)
-                            break;
-                    }
-
-                    })
-                    .error(function(data,status,headers,config){
-                    $scope.retInfo = 'Błąd!'+ data;
-                    })
-
-        $scope.showCompetitionDetails = function(id){
-            sessionStorage.setItem('compID', id);
-            $location.path('/Multi/home/competition');
-        }
-
-
 
     }])
 
